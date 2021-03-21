40dd4756586750c40091117b99ce0c57a8991740
leandrovidal56 <leandrovidal56@gmail.com>
2021-03-20 21:19:12 -0300

    first_commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react'; 
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+
 
 import Splash from '../pages/Splash';
 import Home from '../pages/Home';
